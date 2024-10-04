f4be94cd67ba9a77fdb34f47d06182a35a3d4805
masud3386 <masud3386@yahoo.com>
2024-10-04 09:46:50 -0400

    this is my thirs commit

---
+++ src/github/Testing.java
@@ -0,0 +1,30 @@
+package github;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		
+		
+		byte age=121;
+		if(age>=21) {
+			System.out.println("A person is eligible to register vote");
+		}else {
+			System.out.println("A person is not eligible to register vote");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
